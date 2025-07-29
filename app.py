645758ed3ceb3ceacba7a449c8944b2860552d2e
SpencerHawk <spha8949@colorado.edu>
2025-07-29 23:36:38 +0000

    db_create route added

--- app.py
+++ app.py
@@ -12,3 +12,20 @@ def db_test():
     conn = psycopg.connect("postgresql://spencer_db_1vyf_user:hllDG7XjGhJb5TGJAkrBooEwq7ltwX10@dpg-d24lc4s9c44c73aet560-a/spencer_db_1vyf")
     conn.close()
     return "Database connection successful."
+
+@app.route('/db_create')
+def db_create():
+    conn = psycopg.connect("postgresql://spencer_db_1vyf_user:hllDG7XjGhJb5TGJAkrBooEwq7ltwX10@dpg-d24lc4s9c44c73aet560-a/spencer_db_1vyf")
+    cur = conn.cursor()
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS Basketball (
+            First varchar(255),
+            Last varchar(255),
+            City varchar(255),
+            Name varchar(255),
+            Number int
+            );
+    ''')
+    conn.commit()
+    conn.close()
+    return "Basketball table successfully created."
