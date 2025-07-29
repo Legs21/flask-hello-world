92c2e3b29308189b835b13a2bf7288c7c6a43d0a
SpencerHawk <spha8949@colorado.edu>
2025-07-29 23:18:52 +0000

    db_test page created - linked to database

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
+import psycopg2
+
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hello World from Spencer Hawkins in 3308!'
+
+@app.route('/db_test')
+def db_test():
+    conn = psycopg2.connect("postgresql://spencer_db_1vyf_user:hllDG7XjGhJb5TGJAkrBooEwq7ltwX10@dpg-d24lc4s9c44c73aet560-a/spencer_db_1vyf")
+    conn.close()
+    return "Database connection successful."
